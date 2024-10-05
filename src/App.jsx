6ced793f7379450b514da285a4831994c359333d
Bishnu Rijal <bishnurijal@Bishnus-Mini.attlocal.net>
2024-10-05 01:38:45 -0500

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState,useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
@@ -8,15 +8,20 @@ import Skills from './components/Skills.jsx';
 import Contact from './components/Contact';
 
 const App = () => {
-return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </div>
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Hero} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/contact" component={Contact} />
+          {/* You can add more routes here */}
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
